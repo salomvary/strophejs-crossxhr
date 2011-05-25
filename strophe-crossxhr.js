d28c7c9d111b3c59306d498b9a8cef811aaa7bf8
Márton Salomváry <salomvary@gmail.com>
2011-05-25 10:49:50 +0200

    added IE compatibility

--- strophe-crossxhr.js
+++ strophe-crossxhr.js
@@ -1,24 +1,38 @@
 (function() {
+	//XXX weird but required for IE
+	document.domain = document.domain;
+
 	var newXHR;
 
+	/**
+	 * Initializes crossdomain XMLHttpRequest.
+	 *
+	 * @param {String} src URL of newxhr.html
+	 * @param {Function} [success] called on successful IFRAME load
+	 * @param {Function} [error] called on failed IFRAME load
+	 */
 	Strophe.crossXHR = function(src, success, error) {
-		var iframe = document.createElement('iframe');
-		iframe.onload = function() {
-			var err;
-			try {
-				newXHR = this.contentWindow.newXHR;
-			} catch(e) {
-				err = e;
-			}
-			if(! newXHR) {
-				error && error(err);
-			} else {
-				success && success();
-			}
-		};
-		iframe.src = src;
-		iframe.setAttribute('style', 'display:none');
-		document.body.appendChild(iframe);
+		if(! newXHR) {
+			var iframe = document.createElement('iframe');
+			iframe.onload = function() {
+				var err;
+				try {
+					newXHR = this.contentWindow.newXHR;
+				} catch(e) {
+					err = e;
+				}
+				if(! newXHR) {
+					error && error(err);
+				} else {
+					success && success();
+				}
+			};
+			iframe.src = src;
+			iframe.style.display = 'none';
+			document.body.appendChild(iframe);
+		} else {
+			success();
+		}
 	};
 
 	Strophe.Request.prototype._newXHR = function () {
